4652e3fc352c5495e8a2fa069d9ba6f263745538
Randy Tolentino <makertolentino@gmail.com>
2015-06-02 09:20:18 -0500

    tasks array

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify');
 
-// Default Gulp task
-// Uglifies JS code
-gulp.task('default', function(){
+// Scripts task
+// Uglifies
+gulp.task('scripts', function(){
 	gulp.src('js/*.js')
 	.pipe(uglify())
-	.pipe(gulp.dest('minjs'));
+	.pipe(gulp.dest('build/js'));
 });
+
+// Styles task
+// 
+gulp.task('styles', function(){
+	console.log('run styles');
+});
+
+
+gulp.task('default', ['scripts', 'styles']);
